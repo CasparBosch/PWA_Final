2de12ef30196d9b921302bdce262e3b192485796
CasparBosch <1004288@hr.nl>
2024-02-26 12:42:32 +0100

    voor de her

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,8 @@
-const cacheName = 'v1';
+// const localforage = require("localforage");
+
+importScripts("./src/js/localforage.min.js");
+
+const cacheName = "v1";
 
 // // Index of Pages
 // const cacheAssets = [
@@ -11,61 +15,134 @@ const cacheName = 'v1';
 // ]
 
 // Call Install Event
-self.addEventListener('install', event => {
-    console.log('Service Worker: Installed');
+self.addEventListener("install", (event) => {
+  console.log("Service Worker: Installed");
 
-    // // Cache Content of Indexed Pages
-    // event.waitUntil(
-    //     caches
-    //         .open(cacheName)
-    //         .then(cache => {
-    //             console.log('Service Worker: Caching Files');
-    //             cache.addAll(cacheAssets);
-    //         })
-    //         .then(() => self.skipWaiting())
-    // );
-})
+  // // Cache Content of Indexed Pages
+  // event.waitUntil(
+  //     caches
+  //         .open(cacheName)
+  //         .then(cache => {
+  //             console.log('Service Worker: Caching Files');
+  //             cache.addAll(cacheAssets);
+  //         })
+  //         .then(() => self.skipWaiting())
+  // );
+});
 
 // Call Activate Event
-self.addEventListener('activate', event => {
-    console.log('Service Worker: Activated');
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activated");
 
-    // Remove Unwanted Caches
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if(cache !== cacheName) {
-                        console.log('Service Working: Clearing Old Cache');
-                        return caches.delete(cache)
-                    }
-                })
-            )
+  // Remove Unwanted Caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("Service Working: Clearing Old Cache");
+            return caches.delete(cache);
+          }
         })
-    );
-})
+      );
+    })
+  );
+});
 
 // Call Fetch Event
-self.addEventListener('fetch', event => {
-    console.log('Service Working: Fetching');
+// self.addEventListener("fetch", (event) => {
+//   console.log("Service Working: Fetching");
 
+// console.log(localforage.indexedDB.createInstance());
+
+// const requestClone = event.request.clone();
+
+//   event.respondWith(
+//     // Fetch for Indexed Pages
+//     // fetch(event.request).catch(() => caches.match(event.request));
+
+//     // Fetch for Full Site
+//     fetch(requestClone)
+//       .then((res) => {
+//         //clone response
+//         const responseClone = res.clone();
+
+//         //Tags
+//         if (event.request.url.includes("https://cmgt.hr.nl/api/tags")) {
+//           cache.open(cacheName).then((cache) => {
+//             cache.put(requestClone, responseClone);
+//           });
+//         }
+
+//         //index
+//         if (event.request.url.includes("https://cmgt.hr.nl/api/projects")) {
+//           cache.open(cacheName).then((cache) => {
+//             cache.put(requestClone, responseClone);
+//           });
+//         }
+
+//         // // Make copy/clone of respone
+//         // const resClone = res.clone();
+//         // // Open cache
+//         // caches
+//         //     .open(cacheName)
+//         //     .then(cache => {
+//         //         // Add response to cache
+//         //         cache.put(event.request, resClone);
+//         //     });
+//         return res;
+//       })
+//       .catch((error) => {
+//         console.log(cache)
+//         return caches
+//           .match(requestClone)
+//           .then((res) => res || Promise.reject("no-match"));
+//       })
+//   );
+// });
+
+self.addEventListener("fetch", (event) => {
+  console.log("Service Worker: Fetching");
+
+  // Initialize localforage instance
+  const localforageInstance = localforage.createInstance();
+
+  // Check if the request URL includes the specified endpoint
+  if (event.request.url.includes("https://cmgt.hr.nl/api/tags")) {
+    // Intercept the request and respond accordingly
     event.respondWith(
-        // Fetch for Indexed Pages
-        // fetch(event.request).catch(() => caches.match(event.request));
-
-        // Fetch for Full Site
-        fetch(event.request)
-            .then(res => {
-                // Make copy/clone of respone
-                const resClone = res.clone();
-                // Open cache
-                caches
-                    .open(cacheName)
-                    .then(cache => {
-                        // Add response to cache
-                        cache.put(event.request, resClone);
-                    });
-                return res;
-            }).catch(error => caches.match(event.request).then(res => res))
+      fetch(event.request) // Fetch the request
+        .then((response) => {
+          // Log the response
+          console.log("Response:", response);
+          return response; // Return the response
+        })
+        .catch((error) => {
+          // Log any errors
+          console.error("Error fetching data:", error);
+          // Respond with an error message or fallback response
+          return new Response("An error occurred while fetching data.", {
+            status: 500,
+          });
+        })
     );
-})
+  } else if (event.request.url.includes("https://cmgt.hr.nl/api/projects")) {
+    // Intercept the request and respond accordingly
+    event.respondWith(
+      fetch(event.request) // Fetch the request
+        .then((response) => {
+          // Log the response
+          console.log("Response:", response);
+          return response; // Return the response
+        })
+        .catch((error) => {
+          // Log any errors
+          console.error("Error fetching data:", error);
+          // Respond with an error message or fallback response
+          return new Response("An error occurred while fetching data.", {
+            status: 500,
+          });
+        })
+    );
+  }
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,112 @@
-// Fetch API
-fetch('https://cmgt.hr.nl/api/projects')
-.then(response => response.json())
-.then(data => {
-    const projectsContainer = document.getElementById('projectsContainer');
+// //indexedDB init
+// const request = indexedDB.open("myDatabase", 1);
 
-    // Assuming the API response has an array named 'data' containing project objects
-    // Iterate over each project object
-    data.data.forEach(project => {
-        // Extract required information
-        const title = project.project.title;
-        const tagline = project.project.tagline;
-        const headerImage = project.project.header_image;
+// //upgrade event
+// request.onupgradeneeded = () => {
+//   alert("upgrade needed");
+// };
 
-        // Create HTML elements
-        const projectElement = document.createElement('div');
-        projectElement.classList.add('p-3', 'w-1/4');
-        projectElement.innerHTML = `
-            <div class="rounded-md h-full bg-white shadow">
-                <div class="">
-                    <img src="${headerImage}" alt="${title}" class="w-full h-auto">
-                </div>
-                <div class="p-4">
-                    <h2 class="text-xl font-semibold">${title}</h2>
-                    <p class="text-sm">${tagline}</p>
-                </div>
-            </div>
-        `;
+// //on success
+// request.onsuccess = () => {
+//   alert("success is called");
+// };
 
-        // Append project element to container
-        projectsContainer.appendChild(projectElement);
-    });
-})
-.catch(error => {
-    console.error('Error fetching data:', error);
-});
+// let db;
+
+// const openRequest = indexedDB.open("myDatabase", 1);
+
+// openRequest.onupgradeneeded = function (e) {
+//   db = e.target.result;
+//   console.log("running onupgradeneeded");
+//   const storeOS = db.createObjectStore("myDatabaseStore", { keyPath: "name" });
+// };
+// openRequest.onsuccess = function (e) {
+//   console.log("running onsuccess");
+//   db = e.target.result;
+// };
+// openRequest.onerror = function (e) {
+//   console.log("onerror! doesnt work");
+// };
+
+// // Fetch API
+// fetch("https://cmgt.hr.nl/api/projects")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const projectsContainer = document.getElementById("projectsContainer");
+
+//     // Iterate over each project object
+//     if (document.getElementById("projectsContainer")) {
+//       data.data.forEach((project) => {
+//         // Extract required information
+//         const id = project.project.id; // Get the project ID
+//         const title = project.project.title;
+//         const tagline = project.project.tagline;
+//         const headerImage = project.project.header_image;
+
+//         // Create HTML linked elements
+//         const projectElement = document.createElement("div");
+//         projectElement.classList.add("p-3", "w-1/4");
+//         projectElement.innerHTML = `
+//         <a class="hover:opacity-80" href="detail.html?id=${id}">
+//         <div class="rounded-md h-full bg-white shadow">
+//             <div class="">
+//                 <img src="${headerImage}" alt="${title}" class="w-full h-auto">
+//             </div>
+//             <div class="p-4">
+//                 <h2 class="text-xl font-semibold">${title}</h2>
+//                 <p class="text-sm">${tagline}</p>
+//             </div>
+//         </div>
+//     </a>
+//         `;
+
+//         projectsContainer.appendChild(projectElement);
+//       });
+//     }
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error);
+//   });
+
+// // Project Details
+// // Get the project ID from the URL query parameters
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+// const projectId = urlParams.get("id");
+
+// // Fetch project details based on the project ID
+// fetch("https://cmgt.hr.nl/api/projects")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // Find the project with the matching ID
+//     const project = data.data.find((item) => item.project.id == projectId);
+//     if (project) {
+//       // Display the project details on the page
+//       const detailsContainerElement =
+//         document.getElementById("detailsContainer");
+//       if (detailsContainerElement) {
+//         // Extract project title and display it
+//         const title = project.project.title;
+//         const description = project.project.description;
+//         detailsContainerElement.textContent = title;
+//         detailsContainerElement.textContent = description;
+//       }
+//     } else {
+//       console.error("Project not found.");
+//     }
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching project details:", error);
+//   });
 
 // Service Worker
-if('serviceWorker' in navigator) {
-    console.log('Service Worker is supported')
+if ("serviceWorker" in navigator) {
+  console.log("Service Worker is supported");
 
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-        .register('./../../service-worker.js')
-        .then(reg => console.log('Service Worker: Registered'))
-        .catch(err => console.log(`Service Worker: Error: ${err}`))
-    })
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./../../service-worker.js")
+      .then((reg) => console.log("Service Worker: Registered"))
+      .catch((err) => console.log(`Service Worker: Error: ${err}`));
+  });
 }
